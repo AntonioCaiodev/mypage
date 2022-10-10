cf88887879be3372ef51b056022ffa91c72fcf65
Antonio Carlos de Lima Junior <caiodlj@gmail.com>
2022-10-10 18:02:15 +0000

    ATUALIZAÇÃO 10.10.22

---
+++ teste.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        if(this.scrollY > 200){
+            $('.navbar').addClass("sticky");
+            }else{
+                $('.navbar').removeClass("sticky");
+                }
+
+          // Scrolling Button Btn
+          if(this.scrollY >500){
+            $('.scroll-up-btn').addClass("show");
+            }else{
+            $('.scroll-up-btn').removeClass("show");
+            }   
+    });
+
+    var typed = new Typed(".typing-1",{
+        strings: [
+            "Desenvolvedor" , "Designer", "Criativo", "Curioso"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2",{
+        strings: [
+            "Desenvolvedor" , "Designer", "Criativo", "Curioso"
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    //slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
